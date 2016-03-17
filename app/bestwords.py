0644f337a959c1cdf50ff0974b4f9e4a169dc4f9
Chris Nasrallah <Chris.Nasrallah@mtvstaff.com>
2016-03-17 17:42:02 -0400

    take string argument at command line with -i flag

--- app/bestwords.py
+++ app/bestwords.py
@@ -1,3 +1,4 @@
+import sys, getopt
 import nltk
 import random
 from textblob import TextBlob
@@ -23,6 +24,9 @@ def insert_great(s):
 
 
 def insert_better(s):
+	""" Need to worry about noun modifiers. 
+		'a test sentence' may become 'a test great sentence' 
+		'a good house' might become 'a good great house' """
 	better_words = ['great', 'best', 'huge']
 	the_word = random.choice(better_words)
 	tokens = nltk.word_tokenize(s)
@@ -30,9 +34,10 @@ def insert_better(s):
 	## get the noun indices
 	noun_idx = [idx for idx in range(len(pos_tokens)) if pos_tokens[idx][1] == 'NN']
 	## choose one
-	the_idx = random.choice(noun_idx)
-	## slicing
-	tokens[the_idx:the_idx] = [the_word]
+	if len(noun_idx) > 0:
+		the_idx = random.choice(noun_idx)
+		## slicing
+		tokens[the_idx:the_idx] = [the_word]
 	new_s = ' '.join(tokens)
 	return new_s
 	
@@ -50,17 +55,19 @@ def insert_better(s):
 def insert_stinger(s):
 	neg_stingers = ['Pathetic.', 'Loser.', 'The worst.', 'Dummies.', 'Tough!', 'Sad!', "What a joke."]
 	pos_stingers = ['The best.', 'America.', 'Amazing!']
-	score = TextBlob(s).sentiment.polarity
+	new_s = s
+	## get the sentiment of the sentence and a random number
+	score = TextBlob(new_s).sentiment.polarity
 	r = random.random()
-	stinger = ''
+	## If r is less than abs(score) insert something
 	if r < abs(score): 
 		if (score > 0):
 			## Chose positive stinger
-			stinger = random.choice(pos_stingers)
+			new_s = new_s + ' ' + random.choice(pos_stingers)
 		if (score < 0):
 			## Choose negative stinger
-			stinger = random.choice(neg_stingers)
-	return s + ' ' + stinger
+			new_s = new_s + ' ' + random.choice(neg_stingers)
+	return new_s
 
 
 
@@ -84,16 +91,48 @@ def prepend_social(s):
 	return new_s
 
 
+def test_all_functions(test_string):
+	print('test_string:')
+	print(test_string)
+	print()
+	print('prepend meta-statement:')
+	print(prepend_meta(test_string))
+	print()
+	print('prepend social prof:')
+	print(prepend_social(test_string))
+	print()
+	print('append stinger:')
+	print(insert_stinger(test_string))
+	print()
+	print('insert better:')
+	print(insert_better(test_string))
+	
 
-test_string = "I thought I was good. I was wrong. I'm the best."
-print('test_string:')
-print(test_string)
-print()
-print('prepend meta-statement:')
-print(prepend_meta(test_string))
-print()
-print('prepend social prof:')
-print(prepend_social(test_string))
-print()
-print('append stinger:')
-print(insert_stinger(test_string))
+
+def main(argv):
+
+	command_line_instructions = 'bestwords.py -i <test_string>'
+	try:
+		opts, args = getopt.getopt(argv,"hi:",["config=","param2="])
+	except getopt.GetoptError:
+		print (command_line_instructions)
+		sys.exit()
+	if (len(opts) > 0):	
+		#print args
+		#print opts	
+		for opt, arg in opts:
+			#print opt
+			if opt == '-h':
+				print (command_line_instructions)
+				sys.exit()
+			elif opt in ("-i", "--param1"):
+				test_string = arg
+	else:
+		test_string = "I thought I was good. I was wrong. I'm the best."
+		#test_string = "My friend and I were excited to go to the party."
+	
+	test_all_functions(test_string)
+	
+	
+if __name__ == "__main__":
+	main( sys.argv[1:] ) 
